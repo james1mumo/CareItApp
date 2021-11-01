278e120e873539c251196c4dbad3fdb429176c42
james mumo <james1mumo@gmail.com>
2021-11-01 18:30:22 +0300

    Created Login and registration design files.

--- app/src/main/java/com/dekut/careitapp/MainActivity.java
+++ app/src/main/java/com/dekut/careitapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.dekut.careitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.dekut.careitapp.customer.CustomerHomeActivity;
+import com.dekut.careitapp.customer.CustomerLoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, CustomerLoginActivity.class));
     }
 }
